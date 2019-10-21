8d2ea27705860938a8490182fad86811820b2d7f
rabanibaloch3 <33082461+rabanibaloch3@users.noreply.github.com>
2019-10-21 20:54:50 +0500

    Add files via upload

---
+++ Class Timing Assignment.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   printf("Time table of BSCS 1st Semester \n")
+   printf("1- Monday \n")
+   printf("2- Tuesday \n")
+   printf("3- Wednesday \n")
+   printf("4- Thursday \n")
+   printf("5- Friday \n")
+   printf("6- Saturday \n")
+   printf("7- Sunday \n")
+   printf(" Enter any number from above to get information about class \n")
+
+  scanf("%c", &choice);
+  if (choice == '1')
+  {
+      printf("Today is ICT class. \n")
+      printf("Prof. Asfandyar will be teaching it, the timing of class is from 9:30AM to 12PM")
+  }
+  if (choice == '2')
+  {
+      printf("Today is Programming class. \n")
+      printf("Prof. Imran Ali will be teaching it, the timing of class is from 9:30AM to 12PM")
+  }
+  if (choice == '3')
+  {
+      printf("Today is Applied Physics class. \n")
+      printf("Prof. Shams will be teaching it, the timing of class is from 9:30AM to 12PM")
+  }
+  if (choice == '4')
+  {
+      printf("Today is English class. \n")
+      printf("Prof. Mir Sadaat will be teaching it, the timing of class is from 9AM to 12PM")
+  }
+  if (choice == '5')
+  {
+      printf("Today is Mathematics class. \n")
+      printf("Prof. Asfandyar will be teaching it, the timing of class is from 9AM to 12PM")
+  }
+  if (choice == '6')
+  {
+      printf("There is no class today. \n")
+      printf("It's Holiday")
+  }
+  if (choice == '7')
+  {
+      printf("There is no class today \n")
+      printf("It's Holiday")
+  }
+  return0;
+
+  }
+
+
